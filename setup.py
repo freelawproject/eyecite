c7cc817381a72712e787974a2277308998c1d434
vvilliam <wpalin@gmail.com>
2021-01-15 16:19:49 -0500

    version bump v0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import codecs
 import os
 from setuptools import setup, find_packages
 
-VERSION = "0.0.0"
+VERSION = "0.0.1"
 AUTHOR = "Free Law Project"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
