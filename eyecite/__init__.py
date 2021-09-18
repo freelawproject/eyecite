c78a9f3cf0019f9f48143848ec2e89b9181cf3c5
Matt Dahl <matt.dahl.2013@gmail.com>
2021-09-18 15:48:45 -0700

    Renames find_citations.py to find.py.

--- eyecite/__init__.py
+++ eyecite/__init__.py
@@ -1,6 +1,6 @@
 from .annotate import annotate_citations
 from .clean import clean_text
-from .find_citations import get_citations
+from .find import get_citations
 from .resolve import resolve_citations
 
 __all__ = [
