afc7b174d66832875cbacedb1db3a5a39da02d0f
Samier <samier_saeed@protonmail.com>
2024-03-16 08:58:25 -0700

    ca count test

--- tests/test_get.py
+++ tests/test_get.py
@@ -7,6 +7,7 @@ from tests.assets.bluebook_citations import (
     bluebook_3,
     bluebook_4,
 )
+from tests.assets.ca_citations import memo_test
 
 
 @pytest.mark.parametrize(
@@ -23,4 +24,13 @@ def test_bluebook_count(input_text: str, count: int):
 
     for c in cits:
         print(c)
+    assert len(cits) == count
+
+
+@pytest.mark.parametrize(["input_text", "count"], [(memo_test, 3)])
+def test_california_state_count(input_text: str, count: int):
+    cits = get_citations(input_text)
+    for c in cits:
+        print(c)
+
     assert len(cits) == count
